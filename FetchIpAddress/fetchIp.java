464a20a3a4f2a88c0ae3f980a40243234b109325
Abhranil Bhattacharjee <abhra.tb@gmail.com>
2019-04-11 13:17:34 +0530

    Fetching Ip Address

--- FetchIpAddress/fetchIp.java
+++ FetchIpAddress/fetchIp.java
@@ -7,27 +7,42 @@ import java.*;
 class fetchIp
 {
 
-	public static void main(String args[])throws UnknownHostException
-	{
-		Scanner sc = new Scanner(System.in);
-		try
-		{
-			System.out.println("Enter url for which IP needed : ");
-			String s=sc.nextLine();
+    public static void main(String args[])throws UnknownHostException
+    {
+        Scanner sc = new Scanner(System.in);
+        try
+        {
+            //localhost Address
+            InetAddress l=InetAddress.getLocalHost();
+            System.out.println(printIp(l.toString()));
+            
+            System.out.println("Enter url or Domain Name for which IP needed : ");
+            String s=sc.nextLine();
+            
+            InetAddress ip = InetAddress.getByName(new URL(s).getHost());
+            
+            System.out.println("Public Ip of "+s+" : ");
+            //Showing only the Ip address
+            System.out.println(printIp(ip.toString()));
 
-			InetAddress ip = InetAddress.getByName(new URL(s).getHost());
-			
+            // When a particular Address corresponds to many machines
+            System.out.println("Enter url or Domain Name for which IP needed : ");
+            String s1=sc.nextLine();
+            
+            InetAddress getAllIp[] = InetAddress.getAllByName(new URL(s1).getHost());
+            for(InetAddress str: getAllIp)
+            System.out.println(printIp(str.toString()));
+            
 
-			System.out.println("Public Ip of "+s+" : ");
-			//Showing only the Ip address
-			String ipSeg[]=ip.toString().split("/");
-			System.out.println(ipSeg[ipSeg.length-1]);
-		}
-		catch(MalformedURLException e)
-		{
-			System.out.println("Given URL is invalid");
-		}
-	}
+        }
+        catch(MalformedURLException e)
+        {
+            System.out.println("Given URL is invalid");
+        }
+    }
+    public static String printIp(String s)
+    {
+        String ipSeg[]=s.split("/");
+        return (ipSeg[ipSeg.length-1]);
+    }
 }
-
-
